481b0c35a466f11e72a9bce79d6196f5273d95a4
woddyowl <107232768+woddyowl@users.noreply.github.com>
2023-10-09 10:31:53 -0700

    fixed @media so its compatible with cellphone

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,32 @@
-import "../App.css";
+import React, { useState, useEffect } from 'react';
 import Home from "./Home";
 import About from "./About";
 import Resume from "./Resume";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import "../App.css";
 
 export default function Display() {
+  const [padding, setPadding] = useState('25%');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setPadding('0');
+      } else {
+        setPadding('25%');
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="pl-[25%]    w-screen back text-white  ">
+    <div className={`pl-[${padding}] w-screen back text-white`}>
       <Home />
       <About />
       <Resume />
