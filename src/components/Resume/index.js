e5b16baa4d7c57467714a69fd9016ab823e13b43
woddyowl <107232768+woddyowl@users.noreply.github.com>
2023-10-14 14:26:50 -0700

    fix resume description 10/14 2:27:24

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -53,7 +53,7 @@ export default function Resume() {
               </svg>
             </span>
             <h3 class="mb-1 text-lg font-semibold text-white">
-              AS Degree in Computer Science & Mathmatics
+              AS in Computer Science & Mathmatics
             </h3>
             <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
               Completed on May 25th, 2023
@@ -120,7 +120,7 @@ export default function Resume() {
               </svg>
             </span>
             <h3 class="mb-1 text-lg font-semibold text-white">
-            Cisco Virual Experience Program on Forage 
+            Cisco Virual Program on Forage 
             </h3>
             <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
               Completed on Augest 4, 2023
